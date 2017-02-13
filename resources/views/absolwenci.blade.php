c84ddde5657bb839ae813f2331c821385eb5f239
katka-n <katarzynan@gmail.com>
2017-02-13 14:01:12 +0100

    podstrona absolwentow/a, zmiana wyswietlania

--- resources/views/absolwenci.blade.php
+++ resources/views/absolwenci.blade.php
@@ -12,45 +12,28 @@
             <div class="container">
                 <h2 class="center indent">Absolwenci moich kursów</h2>
                 <div class="row">
-                    @for ($i = 0; $i < 4; $i++)
+                    @foreach($graduates as $graduate)
                         <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
                             <div class="thumb-pad2">
                                 <div class="thumbnail">
-                                    <figure><img src="/img/graduates/{{$graduates[$i]['photo']}}" class="thumbnail" alt=""></figure>
+                                    <figure>
+                                        <img src="/img/graduates/{{$graduate['photo']}}" class="thumbnail" alt="">
+                                    </figure>
                                     <div class="caption">
-                                        <p><b>{{$graduates[$i]['name']}}</b></p>
-                                        <p>{{$graduates[$i]['description']}}</p>
-                                        <a href="/absolwent/{{$graduates[$i]['id']}}" class="btn-default btn1">Czytaj
-                                            dalej...</a>
+                                        <p><b>{{$graduate['name']}}</b></p>
+                                        {!!$graduate['description']!!}
+                                        <a href="/absolwent/{{$graduate['id']}}"
+                                           class="btn-default btn1">
+                                            Czytaj dalej...</a>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    @endfor
+                    @endforeach
                 </div>
             </div>
         </div>
-        <div class="offer-box">
-            <div class="container">
-                <
-                <div class="row">
-                    @for ($i = 4; $i < 8; $i++)
-                        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
-                            <div class="thumb-pad2">
-                                <div class="thumbnail">
-                                    <figure><img src="/img/graduates/{{$graduates[$i]['photo']}}" class="thumbnail" alt=""></figure>
-                                    <div class="caption">
-                                        <p><b>{{$graduates[$i]['name']}}</b></p>
-                                        <p>{{$graduates[$i]['description']}}</p>
-                                        <a href="/absolwent/{{$graduates[$i]['id']}}" class="btn-default btn1">Czytaj
-                                            dalej...</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    @endfor
-                </div>
-            </div>
-        </div>
+    </div>
+    </div>
     </div>
 @endsection
--- resources/views/absolwent.blade.php
+++ resources/views/absolwent.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.base-layout')
 
-@section('content')
+@section('pageTitle')Absolwenci - {{$graduate['name']}} @endsection
+@section('description','Od zera do Webdeva. Skorzystaj z naszego szkolenia i zdobądź umiejetności w dziedzinie programowania. Zapraszamy!')
+@section('keywords', 'szkolenia, PHP, JavaScript, CSS, HTML5, kursy programistyczne, szkolenia programistyczne, jak zostać programistą, Kraków, boot camp, Krzysztof Stanio, symfony, angular, laravel, webdeveloper')
 
+@section('content')
     <!--header-->
     <div class="global indent">
         <!--content-->
@@ -28,16 +31,25 @@
                         <div class="thumb-pad2 wow fadeInRight" data-wow-delay="0.4s">
                             <div class="thumbnail">
                                 <div class="caption">
-                                    <h3>Dane kontaktowe</h3>
-                                    <ul class="list1-1">
-                                        <li>e-mail: {{$graduate['contact_mail']}}</li>
-                                        <li>telefon: {{$graduate['contact_phone']}}</li>
-                                        <li><a href="{{$graduate['social_account']}}">Facebook</a></li>
-                                    </ul>
-                                    <h3>Portfolio</h3>
-                                    <ul class="list1-1">
-                                        <li><a href="{!! $graduate['github_account']!!}">Github</a></li>
-                                    </ul>
+                                    @if( ! empty($graduate['contact_mail'] || $graduate['contact_phone'] || $graduate['social_account']))
+                                        <h3>Dane kontaktowe</h3>
+                                        <ul class="list1-1"> @if( ! empty($graduate['contact_mail']))
+                                                <li>e-mail: {{$graduate['contact_mail']}}</li>
+                                            @endif
+                                            @if( ! empty($graduate['contact_phone']))
+                                                <li>telefon: {{$graduate['contact_phone']}}</li>
+                                            @endif
+                                            @if( ! empty($graduate['social_account']))
+                                                <li><a href="{!! $graduate['social_account']!!}">Strona internetowa</a></li>
+                                            @endif
+                                            @endif
+                                        </ul>
+                                        @if( ! empty($graduate['github_account']))
+                                            <h3>Portfolio</h3>
+                                            <ul class="list1-1">
+                                            <li><a href="{!! $graduate['github_account']!!}">Github</a></li>
+                                            </ul>
+                                        @endif
                                 </div>
                             </div>
                         </div>
