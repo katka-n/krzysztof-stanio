df911acdc2bd842f354cdad10029afe535514c92
katka-n <katarzynan@gmail.com>
2017-02-16 12:25:25 +0100

    widok notki

--- resources/views/blog_notka.blade.php
+++ resources/views/blog_notka.blade.php
@@ -9,6 +9,8 @@
         <!--content-->
         <div class="container">
             <div class="row">
+
+
                 <div class="col-lg-8 col-md-8 col-sm-8 thumb-box4">
                     <h2 class="center indent">blog</h2>
                     <div class="thumb-pad7 clearfix">
@@ -62,114 +64,147 @@
                                 {!! Form::submit(trans('Dodaj'), ['id' => 'add-comment', 'class'=>'btn-default btn1']) !!}
                                 {!! Form::close() !!}
 
+                            </div>
                         </div>
-                    </div>
-                    <h3 style="text-align: center">Wasze Komentarze ({{$commentsNumber}})</h3>
-                    @foreach($comments as $comment)
-                        <div class="comments-container">
-                            <ul class="simple-nested">
-                                <li>
-                                    <div class="comment">
-                                        <img src="{{ asset('img/comment.png') }}" width="45" height="45">
-                                        <p>{{$comment['main']->nick}}</p>
-                                        <em>{{ $comment['main']->created_at->diffForHumans() }}</em>
-                                        <p>{{$comment['main']->comment}}</p>
+                        <h3 style="text-align: center">Wasze Komentarze ({{$commentsNumber}})</h3>
+                        @foreach($comments as $comment)
+                            <div class="comments-container">
+                                <ul class="simple-nested">
+                                    <li>
+                                        <div class="comment">
+                                            <img src="{{ asset('img/comment.png') }}" width="45" height="45">
+                                            <p>{{$comment['main']->nick}}</p>
+                                            <em>{{ $comment['main']->created_at->diffForHumans() }}</em>
+                                            <p>{{$comment['main']->comment}}</p>
 
-                                        {{--Odpowiedz na komentarz--}}
-                                        <div class="dropdown_comments">
-                                            <button class="btn-default btn1" onclick="myFunction({{ $comment['main']->id }})"><b>ODPOWIEDZ</b>
-                                            </button>
-                                            <div style="display:none;"
-                                                 class="dropdown-content myDropdown{{ $comment['main']->id }}">
-                                                <form onsubmit="return validateForm()" class="form-comment-hidden" method="post"
-                                                      action="{{route('blog.savecomments',$posts->id)}}"> {{ csrf_field() }}
-                                                    <div class="form-group">
+                                            {{--Odpowiedz na komentarz--}}
+                                            <div class="dropdown_comments">
+                                                <button class="btn-default btn1"
+                                                        onclick="myFunction({{ $comment['main']->id }})">
+                                                    <b>ODPOWIEDZ</b>
+                                                </button>
+                                                <div style="display:none;"
+                                                     class="dropdown-content myDropdown{{ $comment['main']->id }}">
+                                                    <form onsubmit="return validateForm()" class="form-comment-hidden"
+                                                          method="post"
+                                                          action="{{route('blog.savecomments',$posts->id)}}"> {{ csrf_field() }}
+                                                        <div class="form-group">
                                                 <textarea class="form-control" name="comment" rows="4" cols="110"
                                                           placeholder="Komentarz"></textarea>
-                                                        <input type="hidden" name="comment_id" value="{{$comment['main']->id}}">
-                                                        <input type="hidden" name="post_id"
-                                                               value="{{$comment['main']->posts_id}}">
-                                                        <input style="width: auto" type="text" class="form-control form-nick"
-                                                               name="nick" placeholder="Podpisz się">
-                                                    </div>
-                                                    <button type="submit" class="btn-default btn1"><b>DODAJ</b></button>
-                                                    <h2 class="wow fadeInUp">{{ Session::get('message') }}</h2>
+                                                            <input type="hidden" name="comment_id"
+                                                                   value="{{$comment['main']->id}}">
+                                                            <input type="hidden" name="post_id"
+                                                                   value="{{$comment['main']->posts_id}}">
+                                                            <input style="width: auto" type="text"
+                                                                   class="form-control form-nick"
+                                                                   name="nick" placeholder="Podpisz się">
+                                                        </div>
+                                                        <button type="submit" class="btn-default btn1"><b>DODAJ</b>
+                                                        </button>
+                                                        <h2 class="wow fadeInUp">{{ Session::get('message') }}</h2>
 
-                                                </form>
+                                                    </form>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <ul>
-                                        @if(isSet($comment['child']))
-                                            @foreach($comment['child']  as $child)
-                                                <div class="reply">
-                                                    <p>{{$child->nick}}</p>
-                                                    <em>{{ $child->created_at->diffForHumans() }}</em>
-                                                    <p>{{$child->comment}}</p>
+                                        <ul>
+                                            @if(isSet($comment['child']))
+                                                @foreach($comment['child']  as $child)
+                                                    <div class="reply">
+                                                        <p>{{$child->nick}}</p>
+                                                        <em>{{ $child->created_at->diffForHumans() }}</em>
+                                                        <p>{{$child->comment}}</p>
 
-                                                    {{--Odpowiedz na komentarz--}}
-                                                    <div class="dropdown" style="font-size: 12px">
-                                                        <button class="btn-default btn1" onclick="myFunction({{ $child->id }})">
-                                                            <b>ODPOWIEDZ</b></button>
-                                                        <div style="display:none;"
-                                                             class="dropdown-content myDropdown{{ $child->id }}">
-                                                            <form onsubmit="return validateForm()" class="form-comment-hidden"
-                                                                  method="post"
-                                                                  action="{{route('blog.savecomments',$posts->id)}}"> {{ csrf_field() }}
-                                                                <div class="form-group">
+                                                        {{--Odpowiedz na komentarz--}}
+                                                        <div class="col-lg-4 col-md-4 col-sm-4 thumb-box4">
+                                                            <h2 class="center indent">Kategorie wpisów</h2>
+                                                            <ul class="list1-1 indent">
+                                                                @foreach($categories as $category)
+                                                                    <li>
+                                                                        <a href="/blog/kategoria/{{$category['name']}}">{{$category['name']}}</a>
+                                                                    </li>
+                                                                @endforeach
+                                                            </ul>
+                                                            <h2 class="center indent">Najnowsze wpisy</h2>
+                                                            <ul class="list1-1 indent">
+                                                                @foreach($fiveLastPosts as $post)
+                                                                    <li><a href="/blog/notka/{{$post['slug']}}">{{$post['title']}}</a></li>
+                                                                @endforeach
+                                                            </ul>
+                                                            <h2 class="center indent">Archiwum wpisów</h2>
+                                                            <ul class="list1-1 indent">
+                                                                @foreach($postsByDates as $post)
+                                                                    <li>
+                                                                        <a href="/blog/archiwum/{{$post->year}}/{{$post->month}}">{!! fullMonth($post) !!} {{$post->year}}  </a>
+                                                                    </li>
+                                                                @endforeach
+                                                            </ul>
+                                                        </div>           <div class="dropdown" style="font-size: 12px">
+                                                            <button class="btn-default btn1"
+                                                                    onclick="myFunction({{ $child->id }})">
+                                                                <b>ODPOWIEDZ</b></button>
+                                                            <div style="display:none;"
+                                                                 class="dropdown-content myDropdown{{ $child->id }}">
+                                                                <form onsubmit="return validateForm()"
+                                                                      class="form-comment-hidden"
+                                                                      method="post"
+                                                                      action="{{route('blog.savecomments',$posts->id)}}"> {{ csrf_field() }}
+                                                                    <div class="form-group">
                                                             <textarea class="form-control" name="comment" rows="4"
                                                                       cols="110" placeholder="Komentarz"></textarea>
-                                                                    <input type="hidden" name="comment_id"
-                                                                           value="{{$comment['main']->id}}">
-                                                                    <input type="hidden" name="post_id"
-                                                                           value="{{$child->posts_id}}">
-                                                                    <input style="width: auto" type="text"
-                                                                           class="form-control form-nick" name="nick"
-                                                                           placeholder="Podpisz się">
-                                                                </div>
-                                                                <button type="submit" class="btn-default btn1"><b>DODAJ</b>
-                                                                </button>
-                                                                <h2 class="wow fadeInUp">{{ Session::get('message') }}</h2>
+                                                                        <input type="hidden" name="comment_id"
+                                                                               value="{{$comment['main']->id}}">
+                                                                        <input type="hidden" name="post_id"
+                                                                               value="{{$child->posts_id}}">
+                                                                        <input style="width: auto" type="text"
+                                                                               class="form-control form-nick"
+                                                                               name="nick"
+                                                                               placeholder="Podpisz się">
+                                                                    </div>
+                                                                    <button type="submit" class="btn-default btn1"><b>DODAJ</b>
+                                                                    </button>
+                                                                    <h2 class="wow fadeInUp">{{ Session::get('message') }}</h2>
 
-                                                            </form>
+                                                                </form>
+                                                            </div>
                                                         </div>
+                                                        @endforeach
                                                     </div>
-                                                    @endforeach
-                                                </div>
-                            @endif
-                        </div>
-                        </ul>
-                        </li>
-                        </ul>
-                        </ul>
-                    @endforeach
-                </div>
+                                                    @endif
+                                        </ul>
+                                    </li>
+                                </ul>
+                                </ul>
+                                @endforeach
 
-                <div class="col-lg-4 col-md-4 col-sm-4 thumb-box4">
-                    <h2 class="center indent">Kategorie wpisów</h2>
-                    <ul class="list1-1 indent">
-                        @foreach($categories as $category)
-                            <li><a href="/blog/kategoria/{{$category['name']}}">{{$category['name']}}</a></li>
-                        @endforeach
-                    </ul>
-                    <h2 class="center indent">Najnowsze wpisy</h2>
-                    <ul class="list1-1 indent">
-                        @foreach($fiveLastPosts as $post)
-                            <li><a href="/blog/notka/{{$post['slug']}}">{{$post['title']}}</a></li>
-                        @endforeach
-                    </ul>
-                    <h2 class="center indent">Archiwum wpisów</h2>
-                    <ul class="list1-1 indent">
-                        @foreach($postsByDates as $post)
-                            <li>
-                                <a href="/blog/archiwum/{{$post->year}}/{{$post->month}}">{!! fullMonth($post) !!} {{$post->year}}  </a>
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
+                            </div>
+                    </div>
             </div>
-
+            <div class="col-lg-4 col-md-4 col-sm-4 thumb-box4">
+                <h2 class="center indent">Kategorie wpisów</h2>
+                <ul class="list1-1 indent">
+                    @foreach($categories as $category)
+                        <li>
+                            <a href="/blog/kategoria/{{$category['name']}}">{{$category['name']}}</a>
+                        </li>
+                    @endforeach
+                </ul>
+                <h2 class="center indent">Najnowsze wpisy</h2>
+                <ul class="list1-1 indent">
+                    @foreach($fiveLastPosts as $post)
+                        <li><a href="/blog/notka/{{$post['slug']}}">{{$post['title']}}</a></li>
+                    @endforeach
+                </ul>
+                <h2 class="center indent">Archiwum wpisów</h2>
+                <ul class="list1-1 indent">
+                    @foreach($postsByDates as $post)
+                        <li>
+                            <a href="/blog/archiwum/{{$post->year}}/{{$post->month}}">{!! fullMonth($post) !!} {{$post->year}}  </a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
         </div>
-    </div>
+
     </div>
 @endsection
